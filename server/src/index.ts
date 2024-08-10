148fa1ddcb8d82ff0480085a60a9daf626278605
Azaz <azaz.ali1706@gmail.com>
2024-08-10 23:53:30 +0530

    view engine

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,14 @@ import express, {Application, Request, Response} from "express"
 import "dotenv/config"
 const app:Application=express()
 const PORT= process.env.PORT || 7000
+import path from 'path'
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+// * Set View engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 
 app.get('/', (req:Request, res:Response)=>{
     return res.send("working")
